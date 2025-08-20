ad5c73ba3fbb888e6decb00f02de255403e9eea0
jebin2 <jebineinstein@gmail.com>
2025-08-21 00:07:08 +0530

    grok added

--- test_chat_bot_ui_handler.py
+++ test_chat_bot_ui_handler.py
@@ -1,10 +1,10 @@
-from chat_bot_ui_handler import perplexity_ui_chat, search_google_ai_mode, gemini_ui_chat, meta_ui_chat
+from chat_bot_ui_handler import perplexity_ui_chat, search_google_ai_mode, gemini_ui_chat, meta_ui_chat, grok_ui_chat
 from browser_manager.browser_manager import BrowserConfig
 import os
 
 config = BrowserConfig()
-config.user_data_dir = os.getenv("PROFILE_PATH", None)
-meta_ui_chat(
+config.user_data_dir = os.getenv("PROFILE_PATH_1", None)
+grok_ui_chat(
 	user_prompt = (
 		"Describe what is happening in this video frame as if you're telling a story. "
 		"Focus on the main subjects, their actions, the setting, and any important "
